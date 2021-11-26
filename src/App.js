034cdc0e32e0e339d8093ccdc99587f7ba6a7e52
hmntk <hemanth.anits.85@gmail.com>
2021-11-27 00:57:40 +0530

    set loading

--- src/App.js
+++ src/App.js
@@ -9,16 +9,18 @@ function App() {
     delivery: ""
   }
   const [jokeObj, setJokeObj] = useState(init);
+  const [loading, setLoading] = useState(false);
   const [showDel, setShowDel] = useState(false);
 
   const getJoke = () => {
+    setLoading(true);
     setShowDel(false)
     try {
       axios.get("https://v2.jokeapi.dev/joke/Any")
       .then(({data:{joke, setup, delivery}})=>{
         console.log(joke, setup, delivery);
-        setJokeObj({joke: joke, setup: setup, delivery: delivery}
-          )
+        setJokeObj({joke: joke, setup: setup, delivery: delivery});
+        setLoading(false);
       }
       );
       console.log("joke is",jokeObj)
@@ -28,6 +30,12 @@ function App() {
   }
   
   if (jokeObj.joke !== undefined) {
+    if (loading) {
+      return (<div className={"App"}>
+      <h1>. . . loading</h1>
+      <button onClick={getJoke}>Get Joke</button>
+      </div>)
+    }
     return (
       <div className={"App"}>
       <h1>{jokeObj.joke}</h1>
@@ -35,6 +43,12 @@ function App() {
       </div>
     )
   } else {
+    if (loading) {
+      return (<div className={"App"}>
+      <h1>. . . loading</h1>
+      <button onClick={getJoke}>Get Joke</button>
+      </div>)
+    }
     return (<div className={"App"}><h1>{jokeObj.setup}</h1><hr/>
     {showDel ? <h2>{jokeObj.delivery}</h2> : <button onClick={()=>setShowDel(true)}>Show</button>}
     <hr/>
